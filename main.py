f762b6c329907fc73407d1cb5e5026f7b12c8f2d
xRaphao <asantos.raphael@gmail.com>
2023-04-16 17:44:08 -0300

    feat: removing dodgy images

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import tensorflow as tf 
 import os
+import cv2
+import imghdr
+import numpy as np
+from matplotlib import pyplot as plt
 
 # 1.0 Install dependencies and setup
 # Avoid OOM errors by setting GPU Memory Consumption Growth
@@ -7,3 +11,22 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus: 
     tf.config.experimental.set_memory_growth(gpu,True)
 
+
+tf.config.list_physical_devices('GPU')
+# 1.2 Remove dodgy images
+
+data_dir = 'data' 
+image_exts = ['jpeg','jpg', 'bmp', 'png']
+
+for image_class in os.listdir(data_dir): 
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try: 
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in image_exts: 
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e: 
+            print('Issue with image {}'.format(image_path))
+            # os.remove(image_path)
